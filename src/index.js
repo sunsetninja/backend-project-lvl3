03dfa896132aa7dd3ad1239ca9022e99873100f5
Dmitriy Balashov <mockedcat@gmail.com>
2020-10-10 13:43:58 +0300

    default output directory added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 const urlToFilename = (url) => `${url.replace(/^http?s:\/\//, '').replace(/[^\w]/g, '-')}.html`;
 
-const loadPage = (pageurl, outputdir) => axios.get(pageurl)
+const loadPage = (pageurl, outputdir = process.cwd()) => axios.get(pageurl)
   .then(({ data }) => fs.writeFile(path.join(outputdir, urlToFilename(pageurl)), data));
 
 export default loadPage;
