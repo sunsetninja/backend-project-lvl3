52c2febb3a183097acb0210ce11997029065629c
Dmitriy Balashov <mockedcat@gmail.com>
2020-10-11 22:20:55 +0300

    static files saving implemented

--- __tests__/index.js
+++ __tests__/index.js
@@ -9,22 +9,47 @@ import loadPage from '../src/index.js';
 
 const getFixturePath = (filename) => path.join('__tests__', '__fixtures__', filename);
 
-let expected;
+let responseHtml;
+let expectedHtml;
+let expectedImage;
+let expectedCss;
+let expectedJs;
 beforeAll(async () => {
-  expected = await fs.readFile(getFixturePath('page.html'), 'utf-8');
-  nock('https://ru.hexlet.io/')
-    .get('/courses')
-    .reply(200, expected);
+  responseHtml = await fs.readFile(getFixturePath('page.html'), 'utf-8');
+  expectedHtml = await fs.readFile(getFixturePath('expected.html'), 'utf-8');
+  expectedImage = await fs.readFile(getFixturePath('nodejs.png'));
+  expectedCss = await fs.readFile(getFixturePath('application.css'));
+  expectedJs = await fs.readFile(getFixturePath('runtime.js'));
 });
 
 afterAll(() => {
-  nock.cleanAll();
   nock.enableNetConnect();
 });
 
 let output;
 beforeEach(async () => {
+  nock('https://ru.hexlet.io/')
+    .persist()
+    .get('/courses')
+    .reply(200, responseHtml);
+
+  nock('https://ru.hexlet.io/')
+    .get('/assets/application.css')
+    .replyWithFile(200, getFixturePath('application.css'));
+
+  nock('https://ru.hexlet.io/')
+    .get('/packs/js/runtime.js')
+    .replyWithFile(200, getFixturePath('runtime.js'));
+
+  nock('https://ru.hexlet.io/')
+    .get('/assets/professions/nodejs.png')
+    .replyWithFile(200, getFixturePath('nodejs.png'));
+
   output = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+});
+
+afterEach(() => {
+  nock.cleanAll();
 });
 
 describe('Page loader test', () => {
@@ -32,6 +57,17 @@ describe('Page loader test', () => {
     await loadPage('https://ru.hexlet.io/courses', output);
     const result = await fs.readFile(path.join(output, 'ru-hexlet-io-courses.html'), 'utf-8');
 
-    expect(result).toEqual(expected);
+    expect(result).toEqual(expectedHtml);
+  });
+
+  test('should save static files correctly', async () => {
+    await loadPage('https://ru.hexlet.io/courses', output);
+    const image = await fs.readFile(path.join(output, 'ru-hexlet-io-courses_files', 'ru-hexlet-io-assets-professions-nodejs.png'));
+    const css = await fs.readFile(path.join(output, 'ru-hexlet-io-courses_files', 'ru-hexlet-io-assets-application.css'));
+    const js = await fs.readFile(path.join(output, 'ru-hexlet-io-courses_files', 'ru-hexlet-io-packs-js-runtime.js'));
+
+    expect(image).toEqual(expectedImage);
+    expect(css).toEqual(expectedCss);
+    expect(js).toEqual(expectedJs);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,10 +1,56 @@
 import { promises as fs } from 'fs';
 import axios from 'axios';
 import path from 'path';
+import cheerio from 'cheerio';
+import { URL } from 'url';
 
-const urlToFilename = (url) => `${url.replace(/^http?s:\/\//, '').replace(/[^\w]/g, '-')}.html`;
+const urlToDirname = (url) => `${url.replace(/^http?s:\/\//, '').replace(/[^\w]/g, '-')}`;
+
+const urlToFilename = (url, extention = '') => `${urlToDirname(url)}${extention}`;
+
+const getStaticFullname = (dirname, url) => path.join(dirname, urlToFilename(url.replace(/\.[^/.]+$/, ''), path.extname(url) || '.html'));
+
+const mapping = {
+  img: 'src',
+  script: 'src',
+  link: 'href',
+};
 
 const loadPage = (pageurl, outputdir = process.cwd()) => axios.get(pageurl)
-  .then(({ data }) => fs.writeFile(path.join(outputdir, urlToFilename(pageurl)), data));
+  .then(({ data: html }) => {
+    const $ = cheerio.load(html, { decodeEntities: false });
+    const { origin } = new URL(pageurl);
+    const htmlFilename = urlToFilename(pageurl, '.html');
+    const staticDirname = `${urlToDirname(pageurl)}_files`;
+
+    const promises = Object.keys(mapping).map((tagname) => {
+      const tags = $(tagname);
+
+      const tagsWithUrls = tags
+        .map((_, tag) => ({ tag, url: new URL($(tag).attr(mapping[tagname]), origin) }))
+        .filter((_, { url }) => url.origin === origin)
+        .map((_, { tag, url }) => ({ tag, url: url.toString() }));
+
+      tagsWithUrls.each((_, { tag, url }) => {
+        $(tag).attr(mapping[tagname], getStaticFullname(staticDirname, url));
+      });
+
+      const urls = tagsWithUrls.map((_, { url }) => url).get();
+
+      return Promise.all(
+        urls
+          .map((url) => axios.get(url, { responseType: 'arraybuffer' })
+            .then(({ data }) => {
+              const staticPath = path.join(outputdir, getStaticFullname(staticDirname, url));
+
+              return fs.writeFile(staticPath, data);
+            })),
+      );
+    });
+
+    return fs.writeFile(path.join(outputdir, htmlFilename), $.root().html())
+      .then(() => fs.mkdir(path.join(outputdir, staticDirname)))
+      .then(() => Promise.all(promises));
+  });
 
 export default loadPage;
